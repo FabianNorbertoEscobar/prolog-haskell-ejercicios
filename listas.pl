49bbc6e02c41b5beea5be32876bc0bd0ce154432
FabianNorbertoEscobar <fabiannorbertoescobar@gmail.com>
2017-07-25 19:02:28 -0300

    Listas: cabeza y cola

---
+++ listas.pl
@@ -0,0 +1,12 @@
+%las listas solo tienen dos elementos
+%los elementos son una cabeza y una cola
+%tanto la cabeza como la cola pueden ser listas
+listar(L):-
+    Cabeza = [1,1.1],
+    Cola = [2,[3.1,3.2,3.3,3.4],4,5],
+    L = [Cabeza|Cola].
+
+%cabeza y cola
+dameCabeza([C|L],C).
+dameCola([C|L],L).
+
