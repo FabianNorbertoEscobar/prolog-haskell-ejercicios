156dd1130cb96a0503211e8cfa938b51bb798454
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-07-26 19:55:42 -0300

    Árbol genealógico

---
+++ arbol genealogico.pl
@@ -0,0 +1,46 @@
+%hombres
+hombre(pedro).
+hombre(manuel).
+hombre(arturo).
+
+%mujer
+mujer(maria).
+
+%padre e hijo
+padre(pedro,manuel).
+padre(pedro,arturo).
+padre(pedro,maria).
+
+%niño o niña
+niño(X,Y):-
+    hijo(X,Y).
+niño(X,Y):-
+    hija(X,Y).
+
+%hijo
+hijo(X,Y):-
+    padre(Y,X),
+    hombre(X).
+
+%hija
+hija(X,Y):-
+    padre(Y,X),
+    mujer(X).
+
+%hermanos
+hermano-o-hermana(X,Y):-
+    hermano(X,Y).
+hermano-o-hermana(X,Y):-
+    hermana(X,Y).
+
+%X es hermano de Y
+hermano(X,Y):-
+    hijo(X,Z),
+    niño(Y,Z),
+    X\==Y.
+
+%X es hermana de Y
+hermana(X,Y):-
+    hija(X,Z),
+    niño(Y,Z),
+    X\==Y.
