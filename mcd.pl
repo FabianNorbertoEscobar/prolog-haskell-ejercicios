bf49e78903cdc0be313c063c353710dbc8cbcdd6
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-07-28 17:22:21 -0300

    Mínimo común múltiplo

---
+++ mcd.pl
@@ -0,0 +1,22 @@
+%mínimo común múltiplo
+mcd(_,0,0):-!.
+mcd(_,1,1):-!.
+mcd(A,B,X):-
+    A<B,
+    mcd(B,A,X),!.
+mcd(A,B,X):-
+    N is A-B,
+    N=:=0,
+    X is B,!.
+mcd(A,B,X):-
+    N is A-B,
+    N=:=B,
+    X is B,!.
+mcd(A,B,X):-
+    N is A-B,
+    N<B,
+    mcd(B,N,X),!.
+mcd(A,B,X):-
+    N is A-B,
+    N>B,
+    mcd(A,N,X).
